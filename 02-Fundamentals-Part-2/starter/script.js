e536a15a95ec29e54ec647219c84b932dcab456b
George <Georgedewart21@gmail.com>
2024-02-21 16:28:31 -0500

    a

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,10 +1,25 @@
-let bill = 0;
-
-const calcTip = function(bill) {
-return bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
 }
 
-const bills = [125, 555, 44];
+const bills = [22, 295, 176, 440, 37, 10, 1100, 86, 52]
 
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-console.log(bills,tips);
+const tips = []
+const totals = []
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i])
+  tips.push(tip)
+  totals.push(bills[i] + tip)
+}
+
+function calcAverage(arr) {
+  let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  return sum / arr.length
+}
+
+console.log(calcAverage(totals))
+console.log(calcAverage(tips))
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,16 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+'use strict'
 
+function printForecast(arr) {
+  let days = 0
+  for (let i = 0; i < arr.length; i++) {
+    days++
+    console.log(`${arr[i]}°C in ${days} days`)
+  }
+}
+
+const dataOne = [17, 21, 23]
+const dataTwo = [12, 5, -5, 0, 4]
+
+printForecast(dataOne)
+printForecast(dataTwo)
